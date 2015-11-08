aa0e12448ee7a53af69bfc71d99fc2131169c5c5
Guilherme Lima Pereira <guuuilp@gmail.com>
2015-11-08 15:04:03 -0200

    Incluindo semestre no webservice de média
    A partir de agora, é necessário passar a média como parametro body da
    requisição do webservice de media de disciplina. Isso foi necessário
    para conseguir filtrar as médias das matérias de cada semestre em tab
    view no android.l

--- app/controllers/DisciplinaController.php
+++ app/controllers/DisciplinaController.php
@@ -8,10 +8,13 @@ class DisciplinaController extends BaseController {
 	public function index(){
 		$sth = $this->db->prepare("SELECT * 
                                FROM   vmedia_aluno_disciplina
-			                         WHERE  id_usuario = :id_usuario");
+			                         WHERE  id_usuario = :id_usuario AND
+                                      semestre   = :semestre");
 
     $id_usuario = $this->app->request->post('id_usuario');
+    $semestre = $this->app->request->post('semestre');
 
+    $sth->bindParam(':semestre', $semestre);
     $sth->bindParam(':id_usuario', $id_usuario);
 
 		$sth->execute();
